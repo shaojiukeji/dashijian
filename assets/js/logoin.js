31de7219fa66a1ed14205661ce1e2c2dcb058cd8
shaojiukeji <2033831814@qq.com>
2022-01-16 09:49:44 +0800

    完成个人中心的功能开发

--- assets/js/logoin.js
+++ assets/js/logoin.js
@@ -31,10 +31,44 @@ $(function() {
     }
   })
 
+  // 监听注册表单的提交事件
+  $('#form_reg').on('submit', function(e) {
+    // 1. 阻止默认的提交行为
+    e.preventDefault()
+    // 2. 发起Ajax的POST请求
+    var data = {
+      username: $('#form_reg [name=username]').val(),
+      password: $('#form_reg [name=password]').val()
+    }
+    $.post('/api/reguser', data, function(res) {
+      if (res.status !== 0) {
+        return layer.msg(res.message)
+      }
+      layer.msg('注册成功，请登录！')
+      // 模拟人的点击行为
+      $('#link_login').click()
+    })
+  })
 
-
-
-
-
-
+  // 监听登录表单的提交事件
+  $('#form_login').submit(function(e) {
+    // 阻止默认提交行为
+    e.preventDefault()
+    $.ajax({
+      url: '/api/login',
+      method: 'POST',
+      // 快速获取表单中的数据
+      data: $(this).serialize(),
+      success: function(res) {
+        if (res.status !== 0) {
+          return layer.msg('登录失败！')
+        }
+        layer.msg('登录成功！')
+        // 将登录成功得到的 token 字符串，保存到 localStorage 中
+        localStorage.setItem('token', res.token)
+        // 跳转到后台主页
+        location.href = '/index.html'
+      }
+    })
+  })
 })
